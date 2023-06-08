3d25f0eb70274847c696d951b1204be9872633dd
xrabroff <xrabroff.dmitry@yandex.ru>
2023-06-08 23:37:58 +0300

    Add src as packet in pypojecrt.toml

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='A short description of the project.',
-    author='Your name (or your organization/company/team)',
-    license='MIT',
+    version="0.1.0",
+    description="A short description of the project.",
+    author="Your name (or your organization/company/team)",
+    license="MIT",
 )
---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from .data.make_dataset import make_dataset
+from .data.prepare_data import prepare_data
+from .models.train_model import train_model
+
+
+__all__ = ["make_dataset", "prepare_data", "train_model"]
