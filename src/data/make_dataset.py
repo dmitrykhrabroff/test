ed7c24de32c0086b8ef09fba4ae8da82056228e9
xrabroff <xrabroff.dmitry@yandex.ru>
2023-06-10 08:37:47 +0300

    Add option num_token in cli make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,7 +4,7 @@ import click
 import pandas as pd
 
 
-def cut_texts(text: str, num_words=512) -> str:
+def cut_texts(text: str, num_words: int) -> str:
     """Обрезаем текст до нужного кол-ва символов для экономии памяти
 
     Args:
@@ -23,12 +23,14 @@ def cut_texts(text: str, num_words=512) -> str:
 @click.command()
 @click.argument("input_filepath", type=click.Path(exists=True))
 @click.argument("output_filepath", type=click.Path())
-def make_dataset(input_filepath, output_filepath):
+@click.option("--num_tokens", required=False, type=click.INT)
+def make_dataset(input_filepath: str,
+                  output_filepath: str, num_tokens: int=512):
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
     interim_df = pd.read_csv(input_filepath)
-    interim_df["summary"] = interim_df.summary.map(cut_texts)
+    interim_df["summary"] = interim_df.summary.apply(lambda x: cut_texts(x, num_tokens))
     labels_name = [col for col in interim_df.columns if "encodded_label" in col]
     interim_df[["summary"] + labels_name].to_csv(output_filepath, index=False)
 
